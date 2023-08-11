ed1557f40c69945c928c08baf55a3249071199f7
Roo Thorp <roo.thorp@mongodb.com>
2023-08-11 20:29:33 +0100

    Add iTerm settings

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
+# Symlink nvim folder into ~/.config
 ln -s $PWD/nvim $HOME/.config/nvim
+
+# Import iterm settings into iTerm
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${PWD}/iterm-prefs"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
