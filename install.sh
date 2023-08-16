33d5760998ed8dfae77a47d35af5803d9a284117
Roo Thorp <roo.thorp@mongodb.com>
2023-08-16 13:06:33 +0100

    Use proper iterm prefs file

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 ln -s $PWD/nvim $HOME/.config/nvim
 
 # Import iterm settings into iTerm
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${PWD}/iterm-prefs"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${PWD}/com.googlecode.iterm2.plist"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
